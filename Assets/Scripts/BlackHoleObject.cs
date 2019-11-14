61e76dfd8dfeafae12ca170e996b7abde6d741b6
Marco Zakaria <marcozakaria@yahoo.com>
2019-11-14 23:12:34 +0200

    Update blackhole & Tornado
    Update tornado mesh & shader
    blackhole Sphere Scri[t

---
+++ Assets/Scripts/BlackHoleObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class BlackHoleObject : MonoBehaviour
+{
+    [SerializeField] Material[] materialToUpdate;
+    [Range(0f, 1f)]
+    [SerializeField] float rangeProgress;
+    [Range(0f, 20f)]
+    [SerializeField] float range = 6;
+
+    private void Update()
+    {
+        foreach (Material mat in materialToUpdate)
+        {
+            mat.SetVector("_HolePosition", transform.position);
+            mat.SetFloat("_progress", rangeProgress * 1.2f);
+            mat.SetFloat("_Range", range);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
