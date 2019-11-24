f8bf24d1b425129dc19d59b99e3b971a7f25b606
Marco Zakaria <marcozakaria@yahoo.com>
2019-11-24 19:27:44 +0200

    Vibrating Effect Shader
    Fix water Scene
    Vibrating Effect shader

---
+++ Assets/Shaders/Shader Graph/Effects/EffectsController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EffectsController : MonoBehaviour
+{
+    [SerializeField] Material shakeMaterial;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shakeMaterial.SetFloat("_ShakeTime", Time.time);
+        }
+    }
+}
