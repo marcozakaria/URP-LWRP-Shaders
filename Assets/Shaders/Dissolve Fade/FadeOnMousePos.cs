2f9d65001e42906087f4abd55c59aa645b086be3
Marco Zakaria <marcozakaria@yahoo.com>
2020-10-22 22:16:49 +0200

    Dissolve mouse fade

---
+++ Assets/Shaders/Dissolve Fade/FadeOnMousePos.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FadeOnMousePos : MonoBehaviour
+{
+    [SerializeField] MeshRenderer[] Materials;
+    private Camera mainCamera;
+    private Vector3 newPos;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+ 
+    void Update()
+    {
+
+        newPos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));
+        for (int i = 0; i < Materials.Length; i++)
+        {
+            Materials[i].material.SetVector("_WorldPos", newPos);
+        }
+    }
+}
