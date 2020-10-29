93e29773196b5296711b5a746f542297ff8d7120
Marco Zakaria <marcozakaria@yahoo.com>
2020-10-29 19:39:13 +0200

    Hack Matrix Glitch Effect

---
+++ Assets/Shaders/Hack Effect/MatrixJumpBetweenValues.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MatrixJumpBetweenValues : MonoBehaviour
+{
+    [SerializeField] Vector2 waitTime = new Vector2(0.1f, 0.3f);
+    private Material mat;
+
+    void Start()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+        StartCoroutine(StartGlitch());
+    }
+
+    IEnumerator StartGlitch()
+    {
+        float time;
+        while (true)
+        {
+            time = 0;
+            while (time < 0.1f)
+            {
+                mat.SetFloat("_NoiseLerp", Random.Range(-0.2f, 0.2f));
+                time += Time.deltaTime;
+                yield return null;
+            }
+            yield return new WaitForSeconds(Random.Range(waitTime.x, waitTime.y));
+        }
+    }
+}
